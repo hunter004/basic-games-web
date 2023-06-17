7802d1db3fc0fead9c1ddcd552df29e262258090
hunter004 <hdmoran4@gmail.com>
2023-06-17 13:29:05 -0400

    added basic home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import GameMenu from './components/GameMenu/GameMenu';
+import Home from './components/Home/Home';
 function App() {
   return (
     <div className="App">
       <GameMenu />
+      <Home />
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ src/components/Home/Home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const styles = {
+    home: {
+        position: 'absolute',
+        top:63,
+        bottom: 0, left: 0, right:0,
+        textAlign: 'center',
+        backgroundColor: '#050505',
+        color: '#14d0e0',
+    }
+};
+export default class Home extends React.Component {
+    render() {
+        return <div style={styles.home}>
+            <h1>Home Page</h1>
+            <p>Landing Page of Applications</p>
+        </div>
+    }
+}
