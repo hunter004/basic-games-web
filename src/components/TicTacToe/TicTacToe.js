e705fc893e35068f7d846d66c2f194a011d2621d
hunter004 <hdmoran4@gmail.com>
2023-06-19 21:09:19 -0400

    updates for tictactoe

--- src/components/TicTacToe/TicTacToe.js
+++ src/components/TicTacToe/TicTacToe.js
@@ -8,12 +8,45 @@ const styles = {
         textAlign: 'center',
         backgroundColor: '#050505',
         color: '#14d0e0',
+    },
+    square: {
+        background: '#fff',
+        border: '5px solid #14d0e0',
+        float: 'left',
+        height: '150px',
+        padding: 0,
+        width: '150px',
+        fontSize: '50px',
+        color: '#14d0e0'
+    },
+    row: {
+        clear: 'both',
+        content: '',
+        display: 'table',
+        textAlign: 'center',
+        position: 'relative',
+        top: '25%',left: '35%'
+    },
+    status: {
+        color: '#14d0e0',
+        textAlign: 'center',
+        height: '100px',
+        fontSize: '50px'
+    },
+    button: {
+        color: '#14d0e0',
+        height: '75px',
+        width: '200px',
+        fontSize: '20px',
+    },
+    standard: {
+        paddingTop: '20px'
     }
 };
 
 function Square({value, onSquareClick}) {
     return (
-        <button className="square" onClick={onSquareClick}>
+        <button style={styles.square} onClick={onSquareClick}>
             {value}
         </button>
     )
@@ -33,79 +66,37 @@ function Board({xIsNext, squares, onPlay}) {
         }
         onPlay(nextSquares);
     }
-        const winner = calculateWinner(squares);
-        let status;
-        if(winner) {
-            status = 'Winner: ' + winner;
-        }
-        else {
-            status = 'Next player: ' + (xIsNext ? 'X' : 'O');
-        }
+    const winner = calculateWinner(squares);
+    let status;
+    if(winner) {
+        status = 'Winner: ' + winner;
+    }
+    else {
+        status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+    }
 
-        return (
-            <>
-              <div className="status">{status}</div>
-              <div className="board-row">
-                <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-                <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-                <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-              </div>
-              <div className="board-row">
-                <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-                <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-                <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-              </div>
-              <div className="board-row">
-                <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-                <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-                <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-              </div>
-            </>
-          );
+    return (
+        <>
+            <div style={styles.status}>{status}</div>
+            <div style={styles.row}>
+            <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+            <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+            <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+            </div>
+            <div style={styles.row}>
+            <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+            <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+            <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+            </div>
+            <div style={styles.row}>
+            <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+            <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+            <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+            </div>
+        </>
+        );
 }
 
-export default function TicTacToe() {
-    const [history, setHistory] = useState([Array(9).fill(null)]);
-    const [currentMove, setCurrentMove] = useState(0);
-    const xIsNext = currentMove % 2 === 0;
-    const currentSquares = history[currentMove];
-
-    function handlePlay(nextSquares) {
-        const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
-        setHistory(nextHistory);
-        setCurrentMove(nextHistory.length - 1);
-    }
-
-    function jumpTo(nextMove) {
-        setCurrentMove(nextMove);
-    }
-
-    const moves = history.map((squares, move) => {
-        let description;
-        if (move > 0) {
-          description = 'Go to move #' + move;
-        } else {
-          description = 'Go to game start';
-        }
-        return (
-          <li key={move}>
-            <button onClick={() => jumpTo(move)}>{description}</button>
-          </li>
-        );
-      });
-    
-      return (
-        <div className="game">
-          <div className="game-board">
-            <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
-          </div>
-          <div className="game-info">
-            <ol>{moves}</ol>
-          </div>
-        </div>
-      );
-    }
-    
     function calculateWinner(squares) {
       const lines = [
         [0, 1, 2],
@@ -124,4 +115,47 @@ export default function TicTacToe() {
         }
       }
       return null;
-}
+    }
+
+export default function TicTacToe() {   
+    const [history, setHistory] = useState([Array(9).fill(null)]);
+    const [currentMove, setCurrentMove] = useState(0);
+    const xIsNext = currentMove % 2 === 0;
+    const currentSquares = history[currentMove];
+
+    function handlePlay(nextSquares) {
+        const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+        setHistory(nextHistory);
+        setCurrentMove(nextHistory.length - 1);
+    }
+
+    function jumpTo(nextMove) {
+        setCurrentMove(nextMove);
+    }
+
+    // const moves = history.map((squares, move) => {
+    //     let description;
+    //     if (move > 0) {
+    //       description = 'Go to move #' + move;
+    //     } else {
+    //       description = 'Go to game start';
+    //     }
+    //     return (
+    //       <li key={move}>
+    //         <button onClick={() => jumpTo(move)}>{description}</button>
+    //       </li>
+    //     );
+    //   });
+    
+      return (
+        <div style={styles.home}>
+          <div className="game-board">
+            <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+          </div>
+          <div style={styles.standard}>
+            <button style={styles.button} onClick={() => jumpTo(0)}>New Game</button>
+          </div>
+        </div>
+      );
+    }
+    
