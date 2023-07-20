510767f4e5c7d10f706827fcb162ed13b87d77c0
hunter004 <hdmoran4@gmail.com>
2023-07-20 10:51:52 -0400

    home page animation update

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,20 +1,30 @@
 import React from 'react';
+import './Home.css';
 
 const styles = {
     home: {
         position: 'absolute',
+        margin: '0',
+        alignItems: 'center',
         top:63,
         bottom: 0, left: 0, right:0,
         textAlign: 'center',
         backgroundColor: '#050505',
         color: '#14d0e0',
+        justifyContent: 'center',
+        display: 'flex',
+        overflow: 'hidden'
     }
 };
 export default class Home extends React.Component {
     render() {
-        return <div style={styles.home}>
-            <h1>Home Page</h1>
-            <p>React App hosting simple games!</p>
+        return <>
+        <div style={styles.home}>
+           <div class="sun"></div>
+           <div class="earth">
+            <div class="moon"></div>
+           </div>
         </div>
+        </>
     }
 }
